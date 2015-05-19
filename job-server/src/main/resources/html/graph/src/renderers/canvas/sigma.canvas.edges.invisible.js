2149a279cd3d27b380e886e0889e71568d468eff
Sachin Tyagi <sachin.tyagi@imaginea.com>
2015-05-19 17:43:57 +0530

    Added all the static contect to the resources for now.

---
+++ job-server/src/main/resources/html/graph/src/renderers/canvas/sigma.canvas.edges.invisible.js
@@ -0,0 +1,9 @@
+;(function() {
+  'use strict';
+
+  sigma.utils.pkg('sigma.canvas.edges');
+
+  sigma.canvas.edges.invisible = function(edge, source, target, context, settings) {
+//Do nothing
+  };
+})();
